1abf520ec574941a6b80e11eaa0161562123d2d7
Pranav Kaviraj Reddy <pranavreddy1721@gmail.com>
2026-09-21 18:32:57 +0530

    use dedicated premium header on signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import ProtectedRoute from './components/ProtectedRoute';
 import Landing from './pages/Landing';
@@ -9,9 +9,12 @@ import ScanResults from './pages/ScanResults';
 import ScanHistory from './pages/ScanHistory';
 
 export default function App() {
+  const location = useLocation();
+  const isSignupPage = location.pathname === '/signup';
+
   return (
     <div className="min-h-screen">
-      <Navbar />
+      {!isSignupPage && <Navbar />}
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
